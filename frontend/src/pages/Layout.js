bbdcfcf14a73a4b53dbed4e47bd326ea98596680
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2024-11-26 20:45:00 +0000

    Added navbar for home & login

--- frontend/src/pages/Layout.js
+++ frontend/src/pages/Layout.js
@@ -1,16 +1,18 @@
 import { Outlet, Link } from "react-router-dom";
+import '../styles/pages/Layout.css';
 
 const Layout = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
+      <nav className="navbar">
+        <ul className="navbar-links">
+          <li className="navbar-item">
+            <Link to="/" className="navbar-link">Home</Link>
           </li>
-          <li>
-            <Link to="/login">Login</Link>
+          <li className="navbar-item">
+            <Link to="/login" className="navbar-link">Login</Link>
           </li>
+          {/* You can add more links here as needed */}
         </ul>
       </nav>
 
