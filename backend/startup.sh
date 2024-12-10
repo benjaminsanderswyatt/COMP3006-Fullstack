b9a8e6f432f371ea0e5277be0a75965a28db612d
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2024-12-10 12:01:58 +0000

    Updated websockets

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 set -e # Exit on error
 
+
 echo "Installing dependencies..."
-npm install cors express mongoose bcryptjs jsonwebtoken ws
+
+echo "Installing cors"
+npm install cors     
+
+echo "Installing express"
+npm install express
+
+echo "Installing mongoose"
+npm install mongoose
+
+echo "Installing bcryptjs"
+npm install bcryptjs
+
+echo "Installing jsonwebtoken"
+npm install jsonwebtoken
+
+echo "Installing socket.io-client"
+npm install socket.io-client
 
 
 echo "Starting server..."
