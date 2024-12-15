cf517d1e5e1b7d5b8500a307b571d95dc613357d
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2024-12-15 13:56:06 +0000

    Intergration tests now using the correct server app

--- backend/server.js
+++ backend/server.js
@@ -45,3 +45,5 @@ app.get("/hello", function(request, response){
 // Start Server
 const PORT = process.env.PORT || 9000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = { app, server };
--- backend/tests/integrationTests/testIntegration.js
+++ backend/tests/integrationTests/testIntegration.js
@@ -1,11 +1,11 @@
 let chai = require("chai");
 let chaiHttp = require("chai-http");
-let server = require("../../server");
+let {app, server} = require("../../server");
 chai.use(chaiHttp);
     suite("Suite routes", function() {
         test("Test GET /hello", function() {
-            let app = server.app; // Arrange.
-            chai.request(app).get("/hello") // Act.
+            let serverApp = app; // Arrange.
+            chai.request(serverApp).get("/hello") // Act.
             .end(function(error, response) {
             chai.assert.equal(response.status, 200); // Assert.
         });
