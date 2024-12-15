9064213d3f468048ff6a5d8ff823985deb20e771
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2024-12-15 13:48:14 +0000

    Check to see if tests fail without database

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,7 +8,7 @@ const connectDB = async () => {
     console.log('MongoDB connected');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
-    process.exit(1);
+    //process.exit(1);
   }
 };
 
