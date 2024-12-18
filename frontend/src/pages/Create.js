41096bcdde5db5113ff83662cc7441b893c476f2
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2024-12-18 17:14:17 +0000

    Fixed AddItems naming not being discoverded

---
+++ frontend/src/pages/Create.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Create = () => {
+    
+    return (
+        <div style={styles.main}>
+            <h1>Create</h1>
+        </div>
+    );
+};
+
+const styles = {
+    main: {
+        padding: '20px',
+        textAlign: 'center',
+    },
+};
+
+
+export default Create;
