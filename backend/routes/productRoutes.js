218d393a5c238b58405c364e579deed5ae469211
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2025-01-03 01:21:59 +0000

    Added routes to fix tests

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { verifyToken } = require('../middleware/authJWT');
-const { addProduct, getProducts, getMyProducts, cartProducts, setStock, removeProduct } = require('../controllers/productControllers');
+const { addProduct, getProducts, getMyProducts, cartProducts, setStock, removeProduct, buyCart } = require('../controllers/productControllers');
 
 
 const router = express.Router();
@@ -13,6 +13,9 @@ router.get('/get', verifyToken, getProducts);
 // ---------------------- Cart ----------------------
 
 router.post('/cart', verifyToken, cartProducts);
+
+router.post('/buycart', verifyToken, buyCart);
+
 
 // ---------------------- My Products ----------------------
 
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { verifyToken } = require('../middleware/authJWT');
 const { registerUser, loginUser } = require('../controllers/userController');
 
 const router = express.Router();
@@ -6,5 +7,7 @@ const router = express.Router();
 router.post('/register', registerUser);
 
 router.post('/login', loginUser);
+
+router.put('/update', verifyToken, updateUser);
 
 module.exports = router;
