717ca9ae03ee0f2a012040d79e74cc5e259f6a00
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2025-01-03 01:45:44 +0000

    Frontend update user fetch

--- frontend/src/api/fetchUsers.js
+++ frontend/src/api/fetchUsers.js
@@ -46,3 +46,33 @@ export const login = async (email, password) => {
     return { success: false, message: error.message };
   }
 };
+
+
+export const updateUser = async (username, email, password) => {
+  try {
+    const token = localStorage.getItem('token');
+
+    const response = await fetch(`${API_URL}/update`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`, // Include the token in the Authorization header
+      },
+      body: JSON.stringify({ username, email, password }), // Username, email & password can be null
+    });
+
+    const responseJson = await response.json();
+
+    if (!response.ok) {
+      throw new Error(responseJson.message || "Failed to update user");
+    }
+
+    return { success: true, message: responseJson.message };
+
+  } catch (error) {
+    return { success: false, message: error.message };
+  }
+
+
+  
+};
