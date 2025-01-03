7e28507ee227b68139fb6c358ef48a1af19db83c
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2025-01-03 20:50:20 +0000

    Some styling for error page

--- frontend/src/pages/NoPage.js
+++ frontend/src/pages/NoPage.js
@@ -1,6 +1,17 @@
 const NoPage = () => {
-    return <h1>404</h1>;
+    return (
+      <div style={styles.main}>
+        <h1>Error: 404</h1>
+        <h3>Could not find page</h3>
+      </div>
+    );
   };
+
+  const styles = {
+    main: {
+      justifyItems: 'center',
+    }
+  }
   
   export default NoPage;
   
